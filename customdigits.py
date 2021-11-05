5648f4bf751dc7e00690e8444dcf12e40aa4e8b0
Karen Petrosyan <kar.petrosyan03@gmail.com>
2021-11-05 15:06:09 +0400

    final_wo_optim

--- customdigits.py
+++ customdigits.py
@@ -3,51 +3,68 @@ import time
 
 
 def binarydivision(n1, n2, x):
-	beforepotoints = len(str(int(n1)//int(n2)))
-	n1, n2 = str(n1), str(n2)
 
+	tg = 0
+	while float(n1) < float(n2):
+		n1 = float(n1)*10
+		tg+=1
+	n1, n2 = str(n1), str(n2)
+	last = False
 	m = max((len(n1) - n1.find('.') if '.' in n1 else 0), (len(n2) - n2.find('.') if '.' in n2 else 0))
-	print(float((int(float((n1)))*10**(m-1))))
-	n1 = str(int(float((int(float((n1)))*10**(m-1)))))
-	n2 = str(int(float((int(float((n2)))*10**(m-1)))))
-	print(n1, n2, 3)
+	n1 = str(int(float(((float((n1)))*10**(m)))))
+	n2 = str(int(float(((float((n2)))*10**(m)))))
+
 
 	result = ''
 	point = True
 	i1 = 1
 	transfer = True
-
 	tointtarget = int(n1)//int(n2)
-
 	while int(n1[:i1]) < int(n2):
 		i1+=1
-		
-
+	
 	kk = True
- 
-	while len(result) + beforepotoints != x:
+	fakecount = False
+	count = 0
+	afterpoint = False
+	while count != x:
 		curnum = int(n1[:i1]) if kk else int(n1)
 		kk = False
 
 		if curnum >= int(n2):
-
+			last = False
 			tominus = curnum//int(n2) * int(n2)
 			result += str(tominus // int(n2))
-			toadd = curnum - tominus
-		
+			lastadd = str(tominus // int(n2))
+			toadd = curnum - tominus		
 			n1 = str(toadd) + n1[i1:]
-
 			if int(n2) > int(n1) and '.' not in result:
 				result+='.'
-			
+				afterpoint = True
+				
 		else:
+			if last:
+				result += '0'
+				fakecount+=1
+				last=False
 			if str(curnum) == '0' * len(str(curnum)):
-				return result
+				return result/10**tg
 			i1 += 1
 			n1 += '0'
-	return result
+			last = True
 
-print(binarydivision(100.7, 17, 30))
-print(format(100.12512512489/17, '.30f'))
+		if fakecount and afterpoint:
+			count += fakecount
+			fakecount = 0
+			
+	pointind = result.find('.')
+
+	targetpointind = pointind-tg
+	result = result[:targetpointind] + '.' + result[targetpointind:pointind] + result[pointind+1:]
+	return ('0' if result[0] == '.' else '') + result
+	# return result[:pointind] + result[pointind+1:pointind+1+tg] + '.' + result[pointind+1+tg:]
+
+print(binarydivision(20.675747, 30.675747, 5000), 'asd')
+
 
 
