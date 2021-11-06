be192e893f88b85970467f31ac5949fd034b0f72
karosis88 <kar.petrosyanpy@gmail.com>
2021-11-06 16:23:51 +0400

    'withexe'

--- customdigits.py
+++ customdigits.py
@@ -1,70 +1,61 @@
 import time
 
-
-
-def binarydivision(n1, n2, x):
-
-	tg = 0
-	while float(n1) < float(n2):
-		n1 = float(n1)*10
-		tg+=1
-	n1, n2 = str(n1), str(n2)
-	last = False
-	m = max((len(n1) - n1.find('.') if '.' in n1 else 0), (len(n2) - n2.find('.') if '.' in n2 else 0))
-	n1 = str(int(float(((float((n1)))*10**(m)))))
-	n2 = str(int(float(((float((n2)))*10**(m)))))
-
-
-	result = ''
-	point = True
-	i1 = 1
-	transfer = True
-	tointtarget = int(n1)//int(n2)
-	while int(n1[:i1]) < int(n2):
-		i1+=1
-	
-	kk = True
-	fakecount = False
+def binarydivision(n1:float, n2:float, x:float):
 	count = 0
-	afterpoint = False
-	while count != x:
-		curnum = int(n1[:i1]) if kk else int(n1)
-		kk = False
+	last = False
 
-		if curnum >= int(n2):
-			last = False
-			tominus = curnum//int(n2) * int(n2)
-			result += str(tominus // int(n2))
-			lastadd = str(tominus // int(n2))
-			toadd = curnum - tominus		
-			n1 = str(toadd) + n1[i1:]
-			if int(n2) > int(n1) and '.' not in result:
-				result+='.'
-				afterpoint = True
-				
+	h = max([((len(str(i)) - str(i).find('.') - 1 ) if '.' in str(i) else 0) for i in (n1, n2)])
+
+	n1pointind = n1.find('.')
+	n2pointind = n2.find('.')
+
+	if '.' in n1 and '.' in n2:
+		if len(n1.split('.')[1]) < len(n2.split('.')[1]):
+			n1 += (len(n2.split('.')[1]) - len(n1.split('.')[1])) * '0'
 		else:
-			if last:
-				result += '0'
-				fakecount+=1
-				last=False
-			if str(curnum) == '0' * len(str(curnum)):
-				return result/10**tg
-			i1 += 1
-			n1 += '0'
+			n2 += (len(n1.split('.')[1]) - len(n2.split('.')[1])) * '0'
+
+
+	n1 = (n1[:n1pointind] + n1[n1pointind+1 : n1pointind + h + 1] + '.' + n1[n1pointind + h+1:] if '.' in n1 else str(int(n1) * 10**(h)))
+	n2 = (n2[:n2pointind] + n2[n2pointind+1 : n2pointind + h + 1] + '.' + n2[n2pointind + h+1:] if '.' in n2 else str(int(n2) * 10**(h)))
+	
+	if n1[-1] == '.': n1 = n1[:-1]
+	if n2[-1] == '.': n2 = n2[:-1]
+
+	n1, n2 = int(n1), int(n2)
+
+	startresult =n1//n2
+	result = str(startresult) + '.'
+	n1= int(n1) - startresult* (int(n2))
+	n2 = int(n2)
+
+	while count != x:
+		if n1 >= n2 or last:
+			addres = n1//n2
+			result += str(int(addres))
+			count += 1
+			n1 -= n2*addres
+			last = False
+		else:
+			n1*=10
 			last = True
+	return result
+	
 
-		if fakecount and afterpoint:
-			count += fakecount
-			fakecount = 0
-			
-	pointind = result.find('.')
+while True:
+	try:
+		num1 = input('Number 1: ')
+		num2 = input('Number 2: ')
+		digap = input('Digits After Point: ')
+		print(binarydivision(num1, num2, int(digap)))
+		time.sleep(10)
+	except Exception as e:
+		print(e)
 
-	targetpointind = pointind-tg
-	result = result[:targetpointind] + '.' + result[targetpointind:pointind] + result[pointind+1:]
-	return ('0' if result[0] == '.' else '') + result
-	# return result[:pointind] + result[pointind+1:pointind+1+tg] + '.' + result[pointind+1+tg:]
 
-print(binarydivision(20.675747, 30.675747, 5000), 'asd')
+
+
+	
 
 
 
