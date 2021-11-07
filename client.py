613fa6b12b2c3ac53d4b8f7413acae669ce30cde
karosis88 <92274156+karosis88@users.noreply.github.com>
2021-11-08 03:48:00 +0400

    clientbugfix

---
+++ client.py
@@ -0,0 +1,16 @@
+import socket
+import pycustomdigits
+import time
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('192.168.0.13', 7543))
+
+x = 1
+toaddpi = 0
+while 1:
+    toaddpi = pycustomdigits.supersum(pycustomdigits.superdivision('1', str(x), 50), '-' + pycustomdigits.superdivision('1', str(x+2), 50))
+    sock.send(str(toaddpi).encode())
+    x+=8
+    sock.recv(24)
+    
+
